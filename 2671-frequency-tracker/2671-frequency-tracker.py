2d2f4df803105e59c421dc95dcb6cc9807da9a6c
dagm <dagimyibabe19@gmail.com>
2026-02-09 19:56:20 +0300

    Time: 96 ms (88.06%), Space: 74.5 MB (93.03%) - LeetHub

---
+++ 2671-frequency-tracker/2671-frequency-tracker.py
@@ -0,0 +1,34 @@
+class FrequencyTracker:
+
+    def __init__(self):
+        self.count = {}  # number -> frequency
+        self.freq = {}   # frequency -> how many numbers have this frequency
+
+    def add(self, number: int) -> None:
+        prev = self.count.get(number, 0)
+        nxt = prev + 1
+
+        self.count[number] = nxt
+
+        if prev > 0:
+            self.freq[prev] -= 1
+
+        self.freq[nxt] = self.freq.get(nxt, 0) + 1
+
+    def deleteOne(self, number: int) -> None:
+        if number not in self.count:
+            return
+
+        prev = self.count[number]
+        nxt = prev - 1
+
+        self.freq[prev] -= 1
+
+        if nxt == 0:
+            del self.count[number]
+        else:
+            self.count[number] = nxt
+            self.freq[nxt] = self.freq.get(nxt, 0) + 1
+
+    def hasFrequency(self, frequency: int) -> bool:
+        return self.freq.get(frequency, 0) > 0
