3164a91ad26f2896179d4ded2524ca19f2abb25e
dagm <dagimyibabe19@gmail.com>
2026-02-13 16:26:20 +0300

    Time: 260 ms (52.53%), Space: 13.1 MB (98.73%) - LeetHub

---
+++ 0661-image-smoother/0661-image-smoother.py
@@ -0,0 +1,21 @@
+class Solution:
+    def imageSmoother(self, img):
+        # m * n * 9
+
+        rows, cols = len(img), len(img[0])
+
+        out = [[0] * cols for _ in range(rows)]
+
+        for r in range(rows):
+            for c in range(cols):
+                _count = 0
+                _sum = 0
+                # process the _count and _sum
+
+                for new_row in range(max(0, r - 1), min(rows, r + 2)):
+                    for new_col in range(max(0, c - 1), min(cols, c + 2)):
+                        _count += 1
+                        _sum += img[new_row][new_col]
+
+                out[r][c] = _sum // _count
+        return out
