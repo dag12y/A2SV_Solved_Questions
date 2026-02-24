1c21de2615230b6a5346d2f9de05c059bb631818
dagm <dagimyibabe19@gmail.com>
2026-02-24 17:28:04 +0300

    Time: 73 ms (33.14%), Space: 16.6 MB (16.15%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        people.sort()
+        i, j = 0, len(people) - 1
+        ans = 0
+
+        while i <= j:
+            if people[i] + people[j] <= limit:
+                i += 1  
+            j -= 1     
+            ans += 1 
+        return ans
