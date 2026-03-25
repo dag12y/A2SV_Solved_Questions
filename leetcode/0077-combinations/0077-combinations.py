a8dea35a7208ff178d777edfd2b983ec4cbef35e
dagm <dagimyibabe19@gmail.com>
2026-03-25 11:51:39 +0300

    Add solution for Combinations

---
+++ leetcode/0077-combinations/0077-combinations.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        
+        def backtrack(start, path):
+            # base case
+            if len(path) == k:
+                res.append(path[:])
+                return
+            
+            # try choices
+            for i in range(start, n + 1):
+                path.append(i)           # choose
+                backtrack(i + 1, path)  # go deeper
+                path.pop()              # undo (backtrack)
+        
+        backtrack(1, [])
+        return res
