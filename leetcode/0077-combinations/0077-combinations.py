d2f65cb9df3b6b46e37b64408cb8ed6ba757cacf
dagm <dagimyibabe19@gmail.com>
2026-03-26 18:40:00 +0300

    Add solution for Combinations

--- leetcode/0077-combinations/0077-combinations.py
+++ leetcode/0077-combinations/0077-combinations.py
@@ -15,6 +15,8 @@ class Solution(object):
             
             # try choices
             for i in range(start, n + 1):
+                if len(path) + (n - i + 1) < k:
+                    break
                 path.append(i)           # choose
                 backtrack(i + 1, path)  # go deeper
                 path.pop()              # undo (backtrack)
