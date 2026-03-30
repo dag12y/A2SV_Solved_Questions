7e24bbd32da0a76c911e3cd12b11d290fb2d1d9e
dagm <dagimyibabe19@gmail.com>
2026-03-30 18:32:14 +0300

    Add solution for Letter Combinations of a Phone Number

---
+++ leetcode/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        digit_to_char = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        
+        result = ['']
+        for digit in digits:
+            if digit in digit_to_char:
+                chars = digit_to_char[digit]
+                result = [prefix + char for prefix in result for char in chars]
+        return result
