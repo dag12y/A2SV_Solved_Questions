231e786d48702330f0f4d0df9a1ff42bec197eae
dagm <dagimyibabe19@gmail.com>
2026-04-15 18:44:01 +0300

    Add solution for Set Mismatch

---
+++ leetcode/0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findErrorNums(self, nums):
+        seen = set()
+        duplicate = -1
+        
+        for num in nums:
+            if num in seen:
+                duplicate = num
+            seen.add(num)
+        
+        n = len(nums)
+        diff = sum(nums) - (n*(n+1)//2)
+        missing = duplicate - diff
+        
+        return [duplicate, missing]
